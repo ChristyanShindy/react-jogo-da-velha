9d3f46da81715b301ed1a6e89986a7302e770c66
Christyan Shindy <christyan@sattvaconsultoria.com.br>
2025-05-19 17:46:30 -0300

    inserido icon

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,5 +1,6 @@
 import Title from "../title/Title"
 import Subtitle from "../subtitle/Subtitle"
+import Icon from "../icon/Icon"
 
 import styles from './Header.module.css'
 
@@ -8,8 +9,10 @@ function Header () {
     <div className={styles.header}>
       <Title>Jogo da Velha</Title>
       <Subtitle>Criado por Christyan</Subtitle>
+      <div className={styles.iconContent}>
+        <Icon iconName="github" link="https://github.com/ChristyanShindy"/>
+      </div>
     </div>
-    
   )
 }
 
