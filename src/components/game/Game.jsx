8207a63a767287277fb7d04ce2b02e51728c2021
Christyan Shindy <christyan@sattvaconsultoria.com.br>
2025-05-27 18:41:09 -0300

    inserido lógica campeão, declaração campeão e botão reset

--- src/components/game/Game.jsx
+++ src/components/game/Game.jsx
@@ -1,20 +1,56 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import GameOpition from '../gameOption/GameOption'
+import GameInfo from '../gameInfo/GameInfo'
 import styles from './Game.module.css'
-import Icon from '../icon/Icon'
+
+
+const winnerTable = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6]
+]
 
 function Game () {
   const [gameState, setGameState] = useState(Array(9).fill(0))
-  const [currentPlayer, setCurrentPlayer] = useState(1)
+  const [currentPlayer, setCurrentPlayer] = useState(-1)
+  const [winner, setWinner] = useState(0)
+
   const handleClick = (pos) =>{
-    if(gameState[pos] === 0){
+    if(gameState[pos] === 0 && winner === 0){
       let newGameState = [...gameState] /*copia os valores sem alterar o gameState*/
       newGameState[pos] = currentPlayer
-      setCurrentPlayer(currentPlayer * -1)
+      
       setGameState(newGameState)
     }
   }
+
+  const verifyGame = () => {
+    winnerTable.forEach((line) => {
+      const values = line.map((pos) => gameState[pos])
+      const sum = values.reduce((sum, value) => sum + value)
+      if (sum === 3 || sum === -3){
+        setWinner(sum / 3)
+        console.log("Vencedor")
+      } 
+    })
+  }
+
+  const handleReset = () => {
+    setGameState(Array(9).fill(0))
+    setWinner(0)
+    setCurrentPlayer(-1)
+  }
   
+  useEffect(() => {
+    setCurrentPlayer(currentPlayer * -1)
+    verifyGame()
+  }, [gameState])
+
   return (
     <div className={styles.gameContent}>
       <div className={styles.game}>
@@ -28,15 +64,11 @@ function Game () {
           )
         }
       </div>
-      <div className={styles.gameInfo}>
-        <h4>Próximo a Jogar:</h4>
-        {
-          currentPlayer === 1 && <Icon iconName="circle" />
-        }
-        {
-          currentPlayer === -1 && <Icon iconName="x" />
-        }
-      </div>
+      <GameInfo 
+      currentPlayer= {currentPlayer}
+      winner={winner}
+      onReset={handleReset}
+      />
     </div>
     
   )
